a0e53225c5aab50daa5497847acfd67f3cb4eff4
Sergii Garkusha <328@holbertonschool.com>
2018-03-21 13:14:27 -0700

    Added _getline proto

---
+++ exercises/_getline.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+size_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	if (lineptr == NULL || stream == NULL)
+		return (-1);
+
+	jk
+}
