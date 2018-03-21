0dd456c21ffb8aa373c9f259c6a6c1c7bdc4fc18
Sergii Garkusha <328@holbertonschool.com>
2018-03-21 13:49:22 -0700

    Header file for the project

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _shell_h_
+#define _shell_h_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+int _putchar(char c);
+int _strcmp(const char *s1, const char *s2);
+void exit_handler(char *line, ssize_t length);
+void sig_handler(int sig_num);
+
+#endif
