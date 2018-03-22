fa86edaf477a9ab47667adea11a64e0358e36b53
Sergii Garkusha <328@holbertonschool.com>
2018-03-21 18:08:38 -0700

    Made it!

--- shell.c
+++ shell.c
@@ -15,10 +15,8 @@ int main (int ac, char *av[], char *env[])
 	char *line = NULL;
 	char *err_msg = "No such file or directory";
 	char *tok_string[] = {NULL};
-	int frk;
+	pid_t frk;
 
-	(void) av;
-	(void) env;
 	signal(SIGINT, sig_handler);
 
 	if (ac == 1)
@@ -31,20 +29,38 @@ int main (int ac, char *av[], char *env[])
 
 			if (line[len - 1] == '\n')
 				line[len - 1] = '\0';
-			_strtok(line, len, tok_string);
+
+			_strtok(line, tok_string);
+
+			printf("TS0: %s\n", tok_string[0]);
+
 			frk = fork();
 			if (frk < 0)
-				return (-1);
-			else if (frk == 0)
 			{
+				return (-1);
+			}
+
+			if (frk == 0)
+			{
+				printf("TS0: %s\n", tok_string[0]);
+				printf("Child process has started\n");
+
 				if (execve(tok_string[0], tok_string, NULL) == -1)
+				{
+					printf("fail!");
 					return (-1);
+				}
 			}
 			else
+				printf("Parent process is waiting\n");
 				wait (NULL);
 		}
 	else
+	{
+		(void) av;
+		(void) env;
 		printf("%s\n", err_msg);
-	/* code for the case when our shell was called as program */
+		/* code for the case when our shell was called as program */
+	}
 	return (0);
 }
