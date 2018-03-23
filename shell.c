950857b937a892a6a805c383781f5c3928554f70
Sergii Garkusha <328@holbertonschool.com>
2018-03-23 11:32:04 -0700

    Fixed bug with execve

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * main - the root of the shs
  * @ac: arguments counter
@@ -11,56 +10,45 @@
 int main (int ac, char *av[], char *env[])
 {
 	size_t buf;
-	ssize_t len;
 	char *line = NULL;
+	char **tok_string = NULL;
 	char *err_msg = "No such file or directory";
-	char *tok_string[] = {NULL};
+	/* char *tok_string[] = { "/bin/ls", "-la", NULL }; */
 	pid_t frk;
 
-	signal(SIGINT, sig_handler);
+	/* signal(SIGINT, sig_handler); */
 
 	if (ac == 1)
 		while (true)
 		{
+			line = NULL;
 			printf("($) ");
-			len = getline(&line, &buf, stdin);
+			getline(&line, &buf, stdin);
 
-			exit_handler(line, len);
-
-			if (line[len - 1] == '\n')
-				line[len - 1] = '\0';
-
-			_strtok(line, tok_string);
-
-			printf("TS0: %s\n", tok_string[0]);
-
+			tok_string = _strtok(line);
 			frk = fork();
 			if (frk < 0)
-			{
 				return (-1);
-			}
-
 			if (frk == 0)
 			{
-				printf("TS0: %s\n", tok_string[0]);
-				printf("Child process has started\n");
-
 				if (execve(tok_string[0], tok_string, NULL) == -1)
 				{
-					printf("fail!");
+					printf("%s\n", err_msg);
 					return (-1);
 				}
 			}
 			else
-				printf("Parent process is waiting\n");
+			{
 				wait (NULL);
+			}
 		}
 	else
 	{
 		(void) av;
 		(void) env;
-		printf("%s\n", err_msg);
+		/* printf("%s\n", err_msg); */
 		/* code for the case when our shell was called as program */
 	}
+
 	return (0);
 }
