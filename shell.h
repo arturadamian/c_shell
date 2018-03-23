bfe26057559cf8244375a95211061d8db496b130
Sergii Garkusha <328@holbertonschool.com>
2018-03-23 11:32:50 -0700

    Simplified proto

--- shell.h
+++ shell.h
@@ -13,6 +13,6 @@ int _putchar(char c);
 int _strcmp(const char *s1, const char *s2);
 void exit_handler(char *line, ssize_t length);
 void sig_handler(int sig_num);
-void _strtok(char *string, char **tok_string);
+char **_strtok(char *string);
 
 #endif
