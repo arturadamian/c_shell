7921ceb1fd4eba4ff3039e322de9993cd4c61798
Sergii Garkusha <328@holbertonschool.com>
2018-03-26 15:55:49 -0700

    Replaced string comparsion with new notation

--- exit_handler.c
+++ exit_handler.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 void exit_handler(char *line, ssize_t length) {
-	if (_strcmp(line, "exit") == 0)
+	if (_strcmp(line, "exit", 0, 3))
 	{
 		if (length == -1)
 			_putchar('\n');
 		exit(0);
 	}
 
-	if (line[length - 1] == 0)
+	if (line[length - 1])
 	{
 		_putchar('\n');
 		exit(0);
