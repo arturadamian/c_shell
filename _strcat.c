d1fc64c317304710e2d931436a202a562c10c38e
Sergii Garkusha <328@holbertonschool.com>
2018-03-26 16:02:17 -0700

    Created a string concatenator

---
+++ _strcat.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: what to add to the destination string
+ *
+ * Return: void
+ */
+void _strcat(char *dest, const char *src)
+{
+        int i;
+        int len = _strlen(dest);
+
+        for (i = 0; src[i] != '\0'; i++)
+                dest[len++] = src[i];
+
+        dest[len] = '\0';
+}
