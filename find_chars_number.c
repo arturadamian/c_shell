59dfce0dbf1bdbb8c78976d8ff58e85263ff8a9f
Sergii Garkusha <328@holbertonschool.com>
2018-03-28 20:34:57 -0700

    Added doc

--- find_chars_number.c
+++ find_chars_number.c
@@ -1,7 +1,13 @@
 #include "shell.h"
 
 /**
+ * find_chars_number - helper that finds the number of characters c inside the
+ * string s, starting from the index idx
+ * @s: string where to search
+ * @c: string of characters to search for
+ * @idx: the position in the string from where to start
  *
+ * Return: the number of occurences of charactes from the string `c`
  */
 int find_chars_number(const char *s, const char *c, int idx)
 {
