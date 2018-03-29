d90aaa692dba180b3965195e86b1b8a53e66f4c1
Sergii Garkusha <328@holbertonschool.com>
2018-03-28 20:37:16 -0700

    Decoupled into 2 functions, added doc

--- path_resolver.c
+++ path_resolver.c
@@ -1,6 +1,56 @@
 #include "shell.h"
 
 /**
+ * path_resolve_helper - helper function that resolves
+ * a PATH to the executable. Runs only if PATH global
+ * variable isn't empty
+ * @path: the PATH= string from the environment
+ * @cmd: the command to search for
+ *
+ * Return: resolved path to executable, or command as is
+ * if unable to resolve
+ */
+char *path_resolve_helper(const char *path, char *cmd)
+{
+	int path_len = 0, tok_beg = 0, tok_end = 5;
+	char *tok, *bin;
+	Interval itv;
+
+	path_len = _strlen(path);
+	tok_beg = tok_end + 1; /* separator */
+	itv = find_str_seq(path, ":", tok_beg);
+	tok_end = itv.end;
+
+	tok = malloc((sizeof(char) + 1) * path_len);
+	if (!tok)
+		return (NULL);
+	bin = malloc(sizeof(path) + sizeof(cmd));
+	if (!bin)
+		return (NULL);
+
+	get_str_seq(path, tok_beg, tok_end, &tok);
+	_strcpy(bin, tok);
+	_strcat(bin, "/");
+	_strcat(bin, cmd);
+
+	while (access(bin, F_OK) == -1)
+	{
+		if (path_len == tok_end)
+			return (cmd);
+
+		tok_beg = tok_end + 1; /* separator */
+		itv = find_str_seq(path, ":", tok_beg);
+		tok_end = (itv.end > path_len) ? path_len : itv.end;
+		get_str_seq(path, tok_beg, tok_end, &tok);
+		_strcpy(bin, tok);
+		_strcat(bin, "/");
+		_strcat(bin, cmd);
+	}
+
+	return (bin);
+}
+
+/**
  * path_resolver - resolves the PATH to a commpand `cmd`
  * using the environment `env`
  * @env: the pointer to a environ/argv
@@ -11,49 +61,20 @@
  */
 char *path_resolver(char **env, char *cmd)
 {
-	char *path, *tok, *bin;
+	char *path;
 	int c, path_len = 0, tok_beg = 0, tok_end = 5;
-	Interval itv;
+	char *resolved_path = NULL;
 
 	for (c = 0; env[c]; c++)
 		if (_strcmp("PATH=", env[c], tok_beg, tok_end - 1))
 			path = env[c];
 
 	path_len = _strlen(path);
-	tok_beg = tok_end + 1; /* separator */
-	itv = find_str_seq(path, ":", tok_beg);
-	tok_end = itv.end;
 
-	tok = malloc(sizeof(char) * path_len);
-	if (!tok)
-		return (NULL);
+	if (path_len == 5)
+		exit(1);
 
-	bin = malloc(sizeof(tok) + sizeof(cmd));
-	if (!bin)
-		return (NULL);
+	resolved_path = path_resolve_helper(path, cmd);
 
-	get_str_seq(path, tok_beg, tok_end, &tok);
-
-	_strcpy(bin, tok);
-	_strcat(bin, "/");
-	_strcat(bin, cmd);
-
-	while (access(bin, F_OK) == -1) {
-		/* no more tokens in the path */
-		/* return the command as is */
-		if (path_len == tok_end)
-			return (cmd);
-
-		tok_beg = tok_end + 1; /* separator */
-		itv = find_str_seq(path, ":", tok_beg);
-		tok_end = itv.end;
-
-		get_str_seq(path, tok_beg, tok_end, &tok);
-
-		_strcpy(bin, tok);
-		_strcat(bin, "/");
-		_strcat(bin, cmd);
-	}
-
-	return (bin);
+	return (resolved_path);
 }
