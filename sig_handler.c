269a706e1e81bbd39c381be6f47af599258d4f34
Sergii Garkusha <328@holbertonschool.com>
2018-03-28 20:38:14 -0700

    Added proper signal handler

--- sig_handler.c
+++ sig_handler.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
-
 /**
  * sig_handler - takes care of signals
  * @sig_num: a signal
  */
 void sig_handler(int sig_num)
 {
-	(void) sig_num;
+	if (sig_num == SIGINT)
+		write(STDOUT_FILENO, "\n$ ", 3);
 }
